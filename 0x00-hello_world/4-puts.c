492992609127294b1b5d10826f44cc60a13519b0
Paul Lila <paul.saunyi@gmail.com>
2023-02-16 07:55:15 -0800

    C program print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/*
+ * using the function puts to print a string
+ * puts always creates a new line
+ *
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
