75c78904ddd79a2d26251d5a71d0395ab14402fe
Paul Lila <paul.saunyi@gmail.com>
2023-02-16 09:00:35 -0800

    Various size types in my machine

--- 0x00-hello_world/6-sizes.c
+++ 0x00-hello_world/6-sizes.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * puts - outputting the size of various types in my machine
+ *
+ * Return: Always (0) (success)
+ *
+ */
+int main(void)
+{
+	puts("Size of a char: %ld byte(s)", sizeof(char));
+	puts("Size of an int: %ld byte(s)", sizeof(int));
+	puts("Size of a long int: %ld byte(s)", sizeof(long int));
+	puts("Size of a long long int: %ld byte(S)", sizeof(long long int));
+	puts("Size of a float: %ld byte(s)", sizeof(float));
+	return (0);
+}
