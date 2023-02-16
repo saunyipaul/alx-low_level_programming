b6e6ddeba444c12218e4042d01fcf5250e333cac
Paul Lila <paul.saunyi@gmail.com>
2023-02-16 08:37:53 -0800

    Using printf to output

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printf - outputs a string withut a new line
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
