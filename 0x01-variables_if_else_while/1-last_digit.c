6128f3c6d0ab2d0d47cfe2c73444063dcc16af59
Paul Lila <paul.saunyi@gmail.com>
2023-02-17 04:09:54 -0800

    print single numbers of base 10

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Do while - to do iterations
+ *
+ * Return: Always (0) success
+ *
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
+}
