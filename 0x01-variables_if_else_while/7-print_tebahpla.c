2a7db8eccc77e623b0e3210add40eb0df72fc76e
Paul Lila <paul.saunyi@gmail.com>
2023-02-17 11:10:39 -0800

    reversed alphabets

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * for loop for iteration
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
+
