15e91166b0d058078c75d7f902b06947c15b2b5b
Paul Lila <paul.saunyi@gmail.com>
2023-02-17 15:43:32 -0800

    upper and lower alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * for loops
+ *
+ * Retun: alwaays 0 success
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
