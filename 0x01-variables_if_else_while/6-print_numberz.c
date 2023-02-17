faf625b0c529a64bbd073c7a417cc3692e18a1e9
Paul Lila <paul.saunyi@gmail.com>
2023-02-17 05:06:58 -0800

    using do while and putchar() to print 1-10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * while loop - for iteration
+ *
+ * Return: (0) Always success
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	do
+	{
+		putchar(n + '0');
+		putchar('\n');
+		n++;
+	}
+	while (n < 10);
+	return (0);
+}
