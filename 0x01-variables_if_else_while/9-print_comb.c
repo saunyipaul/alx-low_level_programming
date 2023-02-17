6e34b0558041c80e90836a24ef84b3f08f1b5823
Paul Lila <paul.saunyi@gmail.com>
2023-02-17 14:49:10 -0800

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * for loop
+ *
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 40; n < 49; n++)
+	{
+		putchar(',');
+		putchar(' ');
+		putchar('\n');
+	}
+	return (0);
+}
