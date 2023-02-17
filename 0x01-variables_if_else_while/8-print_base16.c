c8d1dec3b505b679db18af3741b76cf81fabe244
Paul Lila <paul.saunyi@gmail.com>
2023-02-17 05:44:33 -0800

    Hexadecimal for loop

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * do while
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 16; n++)
+	{
+		if (n < 10)
+		{
+			putchar(n + '0');
+		}
+		else
+		{
+			putchar(n - 10 + 'a');
+		}
+	}
+	return (0);
+}
