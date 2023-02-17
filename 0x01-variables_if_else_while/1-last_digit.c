e318005f3b40d01406d2f6f0afc3add4f1728c9e
Paul Lila <paul.saunyi@gmail.com>
2023-02-17 14:11:12 -0800

    if statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else if (n % 10 == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit;
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
 	else
 	{
