082391fe085e5b39b7b269d75c79eb8860ae211a
Paul Lila <paul.saunyi@gmail.com>
2023-02-17 11:48:53 -0800

    alphabets without e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * for loop and if statement and operand !=
+ *
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
