ec29bb03bf95be50272d2312072af60a160c6d72
Paul Lila <paul.saunyi@gmail.com>
2023-02-21 11:45:12 -0800

    Prints
    _putchar

---
+++ 0x02-functions_level_programming/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main Entry point 
+ *
+ * print out _putchar
+ *
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
