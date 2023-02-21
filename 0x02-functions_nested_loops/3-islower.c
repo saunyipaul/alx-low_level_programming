c042096a133075451013469ce6f4a489b2e0d46e
Paul Lila <paul.saunyi@gmail.com>
2023-02-21 14:42:07 -0800

    islower for c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Entry point of function
+ *
+ * 'c': variable
+ *
+ * Return: 1 if c lowercase 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	return (0);
+}
