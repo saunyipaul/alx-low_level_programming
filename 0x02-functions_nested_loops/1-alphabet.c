04d797ecb2eaf4fa2c5d47bb9303eda468e94cf5
Paul Lila <paul.saunyi@gmail.com>
2023-02-21 12:35:38 -0800

    alphabets print

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * print all alphabets
+ *
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
