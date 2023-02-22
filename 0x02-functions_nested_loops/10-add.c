7e06c399810a2e6285a798e857d64620fcce375a
Paul Lila <paul.saunyi@gmail.com>
2023-02-21 17:53:41 -0800

    adding numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - entry point of function add
+ *
+ * @j: variable
+ * @i: variable
+ *
+ * program to add two numbers
+ *
+ * Return: always 0 success
+ */
+
+int add(int j, int i)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+	return (0);
+}
