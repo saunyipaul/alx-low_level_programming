46f9027b05b65eb392c51bdde9829cdc92ece9eb
Paul Lila <paul.saunyi@gmail.com>
2023-02-23 11:54:10 -0800

    diagonal print "

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point of function
+ * @n: variable 
+ * program that prints \ diagonally
+ */
+
+void print_diagonal(int n)
+{
+	int j;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (n == 0 || n < 0)
+			{
+				_putchar('\n');
+			}
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
