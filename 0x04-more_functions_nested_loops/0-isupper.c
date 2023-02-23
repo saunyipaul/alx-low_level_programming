d4341124552cb9b26dbb30ea299739f3b86806a2
Paul Lila <paul.saunyi@gmail.com>
2023-02-23 05:18:13 -0800

    check if uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - Entry point of function
+ *
+ * @c: variable 
+ *
+ * program to check for uppercase letter
+ *
+ * Return: return 1 if c is uppercase otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
