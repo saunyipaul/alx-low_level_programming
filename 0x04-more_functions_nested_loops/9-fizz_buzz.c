8b4b4c227373759d0bf25365ac3e284ac4ed12f0
Paul Lila <paul.saunyi@gmail.com>
2023-02-23 13:14:41 -0800

    Fizz Buzz Fixxbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3) == 0)
 		{
 			printf("Fizz ");
 		}
@@ -20,11 +24,7 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
-		else if ((i % 3) == 0 && (i % 5) == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else 
+		else
 		{
 			printf("%d ", i);
 		}
