36b866a9099f8a9c9d6fd44d6e72870503715265
Paul Lila <paul.saunyi@gmail.com>
2023-02-23 16:06:08 -0800

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -55,4 +55,6 @@ void print_remaining_days(int month, int day, int year)
 		day_of_year += day;
 		printf("Day of the year: %d\n", day_of_year);
 		printf("Remaining days: %d\n", days_in_year - day_of_year);
+		}
+	}
 }
