0d697481f32976123198cdef83c3b281b7eec2fb
Paul Lila <paul.saunyi@gmail.com>
2023-02-28 02:13:02 -0800

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - protype function
+ * @str: variable parameter
+ * program to print a string
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
