2fadaa5afad04f7adce1ba455dbe7a97426bf05f
Paul Lila <paul.saunyi@gmail.com>
2023-02-28 08:46:11 -0800

    prints every character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - start of function prototype
+ * @str: variable parameter
+ * program to print every other character in a string
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
