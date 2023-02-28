4677378f959ac8952275a8af7b470249bb0fa818
Paul Lila <paul.saunyi@gmail.com>
2023-02-28 01:46:51 -0800

    print the string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Entry point of function
+ * @s: variable parameter
+ * program that prints the length of a string
+ * Retun: Always 0 success
+ */
+
+int _strlen(char *s)
+{
+	return(strlen(s));
+}
