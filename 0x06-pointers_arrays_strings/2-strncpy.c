ce2d626e475ebcbafe2c6b6faeb6aa3d0858578c
Paul Lila <paul.saunyi@gmail.com>
2023-03-02 10:17:25 -0800

    function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncpy - Entry point of function prototype
+ * @dest: variable parameter
+ * @src: variable parameter
+ * @n: variable parameter
+ * function that copies a string
+ * Return: always 0 success otherwise 1
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
