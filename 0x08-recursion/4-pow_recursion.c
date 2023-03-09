a4a2bcd67e5ee2c224653acabb7003b91aa4af1d
Paul Lila <paul.saunyi@gmail.com>
2023-03-09 11:31:22 -0800

    recursion for power of an integer

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - function recursion of x power y
+ * @x: parameter integer to power
+ * @y: parameter to power with
+ * 
+ * Return: y < 0 return 1 otherwise 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
