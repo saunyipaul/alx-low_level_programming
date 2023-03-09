5782a1d3033b0b8cdc279a94b7fb15f632d075f2
Paul Lila <paul.saunyi@gmail.com>
2023-03-09 13:01:24 -0800

    prime numbers using recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_prime_number - function to check the prime numbers
+ * @n: parameter
+ * Return: prime number return 1 otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	if (n % 2 == 0)
+	{
+		return (0);
+	}
+	return (is_prime_helper(n, 3));
+}
+
+int is_prime_helper(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return is_prime_helper(n, i + 2);
+}
