1383d0428b43e461c04a1c6915a75deb358d637f
Paul Lila <paul.saunyi@gmail.com>
2023-03-09 09:19:17 -0800

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - entry point of function
+ * @s: varaiable parameter
+ * function that prints a string in recursion
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
