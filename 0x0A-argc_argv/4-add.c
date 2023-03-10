43bcec3ab95de51d6400931a4cbafc32ebfc5d74
Paul Lila <paul.saunyi@gmail.com>
2023-03-10 15:00:15 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/**
+ * main - entry point of prigram main
+ * @argc: number of arguments
+ * @argv: arrays numbers
+ * Return: always 0 success otherwise 1
+ */
+
 int main(int argc, char *argv[])
 {
 	int sum = 0;
@@ -12,14 +19,15 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		arg = argv[i];
-	}
-	for (j = 0; arg[j] != '\0'; j++)
-	{
+		for (j = 0; arg[j] != '\0'; j++)
+		{
 		if (!isdigit(arg[j]))
 		{
 			printf("Error\n");
 			return (1);
 		}
+		}
+		sum += atoi(arg);
 	}
 	printf("%d\n", sum);
 	return (0);
