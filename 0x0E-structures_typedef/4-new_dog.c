f0796554e71aaa32920624448bbf9e68cf7d26d2
Paul Lila <paul.saunyi@gmail.com>
2023-03-20 14:36:48 -0700

    create a new object

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,33 +14,28 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog(char *name, float age, char *owner) {
-    // Allocate memory for the dog structure
-    dog_t *dog = (dog_t *) malloc(sizeof(dog_t));
-    if (!dog) {
-        // Memory allocation failed, return NULL
-        return NULL;
-    }
-    // Allocate memory for the name and copy it
-    dog->name = (char *) malloc(strlen(name) + 1);
-    if (!dog->name) {
-        // Memory allocation failed, clean up and return NULL
-        free(dog);
-        return NULL;
-    }
-    strcpy(dog->name, name);
-    // Allocate memory for the owner and copy it
-    dog->owner = (char *) malloc(strlen(owner) + 1);
-    if (!dog->owner) {
-        // Memory allocation failed, clean up and return NULL
-        free(dog->name);
-        free(dog);
-        return NULL;
-    }
-    strcpy(dog->owner, owner);
-    // Set the age
-    dog->age = age;
-    // Return the new dog
-    return;
+		dog_t *dog = (dog_t *) malloc(sizeof(dog_t));
+
+		if (!dog)
+		{
+			return (NULL);
+		}
+		dog->name = (char *) malloc(strlen(name) + 1);
+		if (!dog->name)
+		{
+			free(dog);
+			return (NULL);
+		}
+		strcpy(dog->name, name);
+		dog->owner = (char *) malloc(strlen(owner) + 1);
+		if (!dog->owner)
+		{
+			free(dog->name);
+			free(dog);
+			return (NULL);
+		}
+		strcpy(dog->owner, owner);
+		dog->age = age;
+		return;
 }
 }
