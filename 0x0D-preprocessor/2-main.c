2c737012556c9c884ce3615f2104256379884369
Paul Lila <paul.saunyi@gmail.com>
2023-03-20 12:30:24 -0700

    program that prints name of file if it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point of function
+ * prints name of file given
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
