14749205b68b80be90b019d44b2234fb19122a49
Paul Lila <paul.saunyi@gmail.com>
2023-03-20 13:25:59 -0700

    function that creates an object using struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,14 +10,14 @@
  *
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }dog_t;
 /* Additional function prototypes */
-int _putchar(char c);
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
