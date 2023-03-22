c5c08eb08eef2877ca7a4962a8ea2157a3889d75
Paul Lila <paul.saunyi@gmail.com>
2023-03-22 03:13:29 -0700

    readme file, files for function print using function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function prototype entry
+ * @name: variable
+ * @f: pointer
+ * function that prints a name uses function pointers
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
