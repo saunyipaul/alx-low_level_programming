e3db796a0ec06cf8cae7de4ac94e9698b3f130b0
Lynton makena <paul.saunyi@gmail.com>
2023-03-30 09:46:02 -0700

    function to free list_t

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,10 +11,11 @@
 void free_list(list_t *head)
 {
 	list_t *node = head;
-	
+
 	while (node != NULL)
 	{
 		list_t *next_node = node->next;
+
 		free(node->str);
 		free(node);
 		node = next_node;
