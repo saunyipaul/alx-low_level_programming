2fea5229b61c9e67bde4fdfaaa2586a28f561fcd
Lynton makena <paul.saunyi@gmail.com>
2023-03-30 09:40:13 -0700

    function to free list_t

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - function prototype
+ * @head: pointer
+ * function that frees list_t
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *node = head;
+	
+	while (node != NULL)
+	{
+		list_t *next_node = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
+}
