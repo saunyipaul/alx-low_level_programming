ffb131f23de437444db3b7c52fac170fdefc2c46
Lynton makena <paul.saunyi@gmail.com>
2023-03-30 07:55:22 -0700

    Read me file, function file and function that prints element in a list_t

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - function prototype
+ * @h: pointer
+ * function that prints all the elements of the function
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
