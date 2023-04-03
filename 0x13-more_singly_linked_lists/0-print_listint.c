cd464e08542318813ab6c24f79da06db4aaafac9
Lynton makena <paul.saunyi@gmail.com>
2023-04-03 08:52:33 -0700

    adding header file, readme file and function to print all elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_listint - prototype function
+ * function that prints all elements of a list
+ * @h: pointer to the element
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *current_node;
+
+	for (current_node = h; current_node != NULL; current_node = current_node->next)
+	{
+		printf("%d\n", current_node->n);
+		count++;
+	}
+	return (count);
+
+}
