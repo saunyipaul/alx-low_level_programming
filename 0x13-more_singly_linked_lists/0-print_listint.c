4ccf15c21b6bf3852a39e051e77cdaf08529e73a
Lynton makena <paul.saunyi@gmail.com>
2023-04-03 17:22:28 -0700

    betty changes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,7 +14,8 @@ size_t print_listint(const listint_t *h)
 	size_t count = 0;
 	const listint_t *current_node;
 
-	for (current_node = h; current_node != NULL; current_node = current_node->next)
+	for (current_node = h; current_node != NULL; current_node =
+			current_node->next)
 	{
 		printf("%d\n", current_node->n);
 		count++;
